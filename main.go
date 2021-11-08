915451724770416bd5cc8cb0fef0c97eccc2e8a5
Yoshi <yoshi@mac-mini.local>
2021-11-08 23:45:25 +0900

    add rest

--- main.go
+++ main.go
@@ -24,6 +24,11 @@ func main() {
 
 	putRouter := sm.Methods(http.MethodPut).Subrouter()
 	putRouter.HandleFunc("/{id:[0-9]+}", ph.UpdateProducts)
+	putRouter.Use(ph.MiddlewareValidateProduct)
+
+	postRouter := sm.Methods(http.MethodPost).Subrouter()
+	postRouter.HandleFunc("/", ph.AddProduct)
+	putRouter.Use(ph.MiddlewareValidateProduct)
 
 	s := &http.Server{
 		Addr:         ":9090",
